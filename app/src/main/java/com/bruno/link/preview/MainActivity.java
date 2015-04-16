8cbef39bc4c26f493016891420f5c494b028686a
Bruno Santos <brunonascimentosantos@gmail.com>
2015-04-16 14:56:16 -0300

    first version using jsoup

--- app/src/main/java/com/bruno/link/preview/MainActivity.java
+++ app/src/main/java/com/bruno/link/preview/MainActivity.java
@@ -1,17 +1,36 @@
 package com.bruno.link.preview;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
+    private static String TAG = MainActivity.class.getSimpleName();
+    private TextView mTitle;
+    private TextView mAuthor;
+    private TextView mDescription;
+    private TextView mImageUrl;
+
+    private EditText mInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        preview();
     }
 
 
@@ -36,4 +55,53 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void initViews(){
+        mInput = (EditText) findViewById(R.id.input);
+        mTitle = (TextView) findViewById(R.id.linkTitle);
+        mAuthor = (TextView) findViewById(R.id.linkAuthor);
+        mDescription = (TextView) findViewById(R.id.linkDescription);
+        mImageUrl = (TextView) findViewById(R.id.linkImageUrl);
+    }
+
+    private void preview(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Document doc = Jsoup.connect(mInput.getText().toString()).userAgent("Mozilla").get();
+                    StringBuffer description=new StringBuffer(), image= new StringBuffer(), author= new StringBuffer();
+                    for(Element meta : doc.select("meta")) {
+                        Log.i(TAG,"Name: " + meta.attr("name") + " - Content: " + meta.attr("content"));
+                        if(meta.attr("name").contains("author")){
+                            author.append(meta.attr("content"));
+                        }else if(meta.attr("name").contains("description")){
+                            Log.i(TAG, "Description: "+ meta.attr("name"));
+                            description.append(meta.attr("content"));
+                        }else if(meta.attr("name").contains("image")){
+                            Log.i(TAG, "ImageURL: "+ meta.attr("name"));
+                            image.append(meta.attr("content"));
+                        }
+                    }
+                    update(doc.title(), author.toString(), description.toString().trim(), image.toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void update(final String title, final String author, final String description, final String image){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mTitle.setText(title);
+                mAuthor.setText(author);
+                mDescription.setText(description);
+                mImageUrl.setText(image);
+            }
+        });
+    }
+
+
 }
